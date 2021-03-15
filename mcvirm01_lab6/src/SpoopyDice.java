8e1d145fe47cbd1eaa06cb290a94eab21c848d52
ryanmcvicker <ryanmcvicker12@gmail.com>
2021-03-15 02:30:44 -0400

    added lab 6

---
+++ mcvirm01_lab6/src/SpoopyDice.java
@@ -0,0 +1,27 @@
+package mcvirm01_lab6;
+import java.util.*;
+import java.io.*;
+
+public class SpoopyDice{
+
+	int determinePlay(){
+
+		//method to check the users input 
+	}
+
+	void playGame(int pg){
+
+		//method to roll the dice and play the game
+	}
+	public static Random rd = new Random();
+	public static void main(String[] args){
+
+			int pg = determinePlay();
+			//coditional to check the users input 
+			if(pg != -5){
+				playGame(pg);
+			}
+			
+	}
+}
+
